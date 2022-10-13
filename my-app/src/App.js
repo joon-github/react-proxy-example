d60024976c5cf8e76a8edaeb48b4f86c8c6f2dd9
beomJoon <104752645+joon-github@users.noreply.github.com>
2022-10-13 13:32:46 +0900

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,70 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import BookTable from './components/BookTable';
-import DisplayBoard from './components/DisplayBoard';
-import CreateBook from './components/CreateBook';
-import { getAllBooks, createBook } from './services/BookService';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import BookTable from "./components/BookTable";
+import DisplayBoard from "./components/DisplayBoard";
+import CreateBook from "./components/CreateBook";
+import { getAllBooks, createBook } from "./services/BookService";
+import Footer from "./components/Footer";
+import { json } from "react-router-dom";
 
-function App () {
-
+function App() {
   const [bookShelf, setBookShelf] = useState({});
   const [books, setBooks] = useState([]);
   const [numberOfBooks, setNumberBooks] = useState(0);
+  const [todo, setTodo] = useState(null);
 
   const handleSubmit = () => {
-      createBook(bookShelf)
-        .then(() => {
-          setNumberBooks(numberOfBooks+1);
-      });
-  }
+    createBook(bookShelf).then(() => {
+      setNumberBooks(numberOfBooks + 1);
+    });
+  };
 
   const getAllBook = () => {
-    getAllBooks()
-      .then(data => {
-        setBooks(data);
-        setNumberBooks(data.length);
-      });
-  }
+    getAllBooks().then((data) => {
+      setBooks(data);
+      setNumberBooks(data.length);
+    });
+  };
 
   const handleOnChangeForm = (e) => {
-      let inputData = bookShelf;
-      if (e.target.name === 'book') {
-        bookShelf.book = e.target.value;
-      } else if (e.target.name === 'category') {
-        bookShelf.category = e.target.value;
-      } else if (e.target.name === 'author') {
-        bookShelf.author = e.target.value;
-      }
-      setBookShelf(inputData);
-  }
+    let inputData = bookShelf;
+    if (e.target.name === "book") {
+      bookShelf.book = e.target.value;
+    } else if (e.target.name === "category") {
+      bookShelf.category = e.target.value;
+    } else if (e.target.name === "author") {
+      bookShelf.author = e.target.value;
+    }
+    setBookShelf(inputData);
+  };
 
-  
+  const testFunc = () => {
+    fetch("/api2/todos")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setTodo(data[0].todo));
+  };
+
   return (
     <div className="main-wrapper">
       <div className="main">
         <Header />
-        <CreateBook 
+        <button onClick={() => testFunc()}>test</button>
+        {todo !== null ? <div>{todo}</div> : null}
+        <CreateBook
           bookShelf={bookShelf}
           onChangeForm={handleOnChangeForm}
           handleSubmit={handleSubmit}
         />
-        <DisplayBoard 
-          numberOfBooks={numberOfBooks} 
-          getAllBook={getAllBook} 
-        />
+        <DisplayBoard
+          numberOfBooks={numberOfBooks}
+          getAllBook={getAllBook}
+        ></DisplayBoard>
+
         <BookTable books={books} />
+
         <Footer />
       </div>
     </div>
